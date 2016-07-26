8ecd1d93acd67b53e448e9699a9a290251ccf6ab
David Rowe <david@ctrlaltstudio.com>
2016-07-26 20:26:58 +1200

    Change Boost to be an automatically downloaded external

--- cmake/modules/FindBoost.cmake
+++ cmake/modules/FindBoost.cmake
@@ -17,15 +17,9 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 
-include("${MACRO_DIR}/HifiLibrarySearchHints.cmake")
-hifi_library_search_hints("BOOST")
-
-find_path(BOOST_INCLUDE_DIRS "boost/noncopyable.hpp" PATH_SUFFIXES include include/boost HINTS ${BOOST_SEARCH_DIRS})
-
-find_library(BOOST_LIBRARY NAMES boost_context-vc120-mt-1_57 PATH_SUFFIXES lib64-msvc-12.0 HINTS ${BOOST_SEARCH_DIRS})
-set(BOOST_LIBRARIES ${BOOST_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BOOST DEFAULT_MSG BOOST_INCLUDE_DIRS BOOST_LIBRARIES)
+if (NOT ANDROID)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(BOOST DEFAULT_MSG BOOST_INCLUDE_DIRS BOOST_LIBRARIES)
+endif()
 
 mark_as_advanced(BOOST_INCLUDE_DIRS BOOST_LIBRARIES BOOST_SEARCH_DIRS)
