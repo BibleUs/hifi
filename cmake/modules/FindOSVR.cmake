df1034301bc4c7e2512848382cf2b7bc5106c762
David Rowe <david@ctrlaltstudio.com>
2016-07-26 18:49:02 +1200

    Change OSVR SDK to be an automatically downloaded external

--- cmake/modules/FindOSVR.cmake
+++ cmake/modules/FindOSVR.cmake
@@ -17,20 +17,9 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 
-include("${MACRO_DIR}/HifiLibrarySearchHints.cmake")
-hifi_library_search_hints("osvr")
-
-find_path(OSVR_INCLUDE_DIRS osvr/ClientKit/ClientKit.h PATH_SUFFIXES include include/osvr include/osvr/ClientKit HINTS ${OSVR_SEARCH_DIRS})
-
-find_library(OSVR_CLIENTKIT_LIBRARY NAMES osvrClientKit PATH_SUFFIXES lib HINTS ${OSVR_SEARCH_DIRS})
-find_library(OSVR_RENDERMANAGER_LIBRARY NAMES osvrRenderManager PATH_SUFFIXES lib HINTS ${OSVR_SEARCH_DIRS})
-set(OSVR_LIBRARIES ${OSVR_CLIENTKIT_LIBRARY} ${OSVR_RENDERMANAGER_LIBRARY})
-
-find_path(OSVR_DLL_PATH osvrClientKit.dll PATH_SUFFIXES bin HINTS ${OSVR_SEARCH_DIRS})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OSVR DEFAULT_MSG OSVR_INCLUDE_DIRS OSVR_LIBRARIES OSVR_DLL_PATH)
-
-add_paths_to_fixup_libs(${OSVR_DLL_PATH})
+if (NOT ANDROID)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(OSVR DEFAULT_MSG OSVR_INCLUDE_DIRS OSVR_LIBRARIES)
+endif()
 
 mark_as_advanced(OSVR_INCLUDE_DIRS OSVR_LIBRARIES OSVR_SEARCH_DIRS)
