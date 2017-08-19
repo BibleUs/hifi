cef727db298ecbde930b69cc964aacbcfa65e1f4
Christoph Haag <haagch@frickel.club>
2017-08-19 22:52:42 +0200

    enable openvr plugin on linux
    also disable threaded submit on non windows platform (very slow and crashes steamvr compositor on archlinux)
    
    v2: Fix all gcc warnings
    v3: exclude mac os x and don't update to OpenVR 1.0.10 with this commit

--- plugins/openvr/src/OpenVrHelpers.h
+++ plugins/openvr/src/OpenVrHelpers.h
@@ -73,7 +73,7 @@ struct PoseData {
     }
 
     void update(const glm::mat4& resetMat) {
-        for (int i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
+        for (uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
             if (!vrPoses[i].bPoseIsValid) {
                 continue;
             }
@@ -84,7 +84,7 @@ struct PoseData {
     }
 
     void resetToInvalid() {
-        for (int i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
+        for (uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++) {
             vrPoses[i].bPoseIsValid = false;
         }
     }
