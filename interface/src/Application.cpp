3005e7091dc6ff276f37dac7f26ea5fd85fc3cbd
Christoph Haag <haagch@frickel.club>
2017-11-21 14:51:51 +0100

    Revert "enable threaded submit"
    This reverts commit dafa49d12b41c49c6b0637748200cf42e00b6683.
    
    it segfaults the SteamVR compositor
    
    See https://github.com/ValveSoftware/SteamVR-for-Linux/issues/78

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -2722,7 +2722,6 @@ void Application::initializeGL() {
 #ifdef DISABLE_QML
     setAttribute(Qt::AA_DontCheckOpenGLContextThreadAffinity);
 #endif
-
     // initialize glut for shape drawing; Qt apparently initializes it on OS X
     if (_isGLInitialized) {
         return;
